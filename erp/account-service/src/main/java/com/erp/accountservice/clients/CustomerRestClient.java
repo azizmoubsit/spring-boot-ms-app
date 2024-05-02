a02b012385ba7b31210f940d23e91f9beb9e491f
aziz <azizmobsit@gmail.com>
2024-05-02 15:49:43 +0100

    add resilience4j to account service

--- erp/account-service/src/main/java/com/erp/accountservice/clients/CustomerRestClient.java
+++ erp/account-service/src/main/java/com/erp/accountservice/clients/CustomerRestClient.java
@@ -8,11 +8,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.erp.accountservice.models.Customer;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerRestClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerService", fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable Long id);
 
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customerService", fallbackMethod = "getDefaultAllCustomers")
     List<Customer> allCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception exception) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setEmail("N/A");
+        customer.setFirstName("N/A");
+        customer.setLastName("N/A");
+
+        return customer;
+    }
+
+    default List<Customer> getDefaultAllCustomers() {
+        return List.of();
+    }
 }
