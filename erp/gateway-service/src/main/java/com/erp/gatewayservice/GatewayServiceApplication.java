63ff22314a7ad07098ac7f9e742c7cbb48ce2682
aziz <azizmobsit@gmail.com>
2024-05-02 11:54:11 +0100

    make gateway work with dynamic routes

--- erp/gateway-service/src/main/java/com/erp/gatewayservice/GatewayServiceApplication.java
+++ erp/gateway-service/src/main/java/com/erp/gatewayservice/GatewayServiceApplication.java
@@ -2,9 +2,10 @@ package com.erp.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
-// import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-// import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -13,8 +14,8 @@ public class GatewayServiceApplication {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
-	// @Bean
-	// DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc) {
-	// 	return new 
-	// }
+	@Bean
+	DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+	}
 }
